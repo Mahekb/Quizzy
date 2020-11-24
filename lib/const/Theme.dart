4dec7d8726915a714b5b1946b26a6f1b32fa6b17
parva barbhaya <parvabarbhaya99@gmail.com>
2020-11-24 15:03:11 +0530

    signin

--- lib/const/Theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Colors
-// final sc_AppBarBackgroundColor = Color(0xFF35C5CF);
-final q_PrimaryColor = Color(0xFF0CFFFF);
-final q_green = Color(0xFF75E93E);
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:quizzy/screens/HomeScreen.dart';
-import 'package:quizzy/screens/SignUpScreen.dart';
+import 'package:login_app/src/app.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: HomeScreen(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
 }
